c459007dd3649c541f4d7348a5b01ff1c4f21896
marta <martampm.94@gmail.com>
2022-04-09 13:05:31 +0100

    form added to js

--- js/scripts.js
+++ js/scripts.js
@@ -158,3 +158,127 @@ function hide() {
   document.getElementById("button").style.display = "none";
 }
 
+//form
+
+const form = document.getElementById("formId");
+const inputs = document.querySelectorAll("#formId input");
+
+const expresions = {
+  name: /^[ a-zA-Z\-\’]+$/, // Letters and it can contain spaces.
+  email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  phone: /^\d{7,14}$/, // 7 to 14 numbers.
+  message: /^[ a-zA-Z\-\’]+$/, // Letters and it can contain spaces.
+};
+
+const areas = {
+  name: false,
+  email: false,
+  phone: false,
+  message: false,
+};
+
+const validateForm = (e) => {
+  /*console.log("executed");-----check is working*/
+  /*console.log(e.target.name);-----check is working*/
+  switch (e.target.name) {
+    case "name":
+      /* console.log("name works!");-----check is working*/
+      validateArea(expresions.name, e.target, "name");
+      break;
+    case "email":
+      /*  console.log("email works!");-----check is working*/
+      validateArea(expresions.email, e.target, "email");
+      break;
+    case "phone":
+      /*  console.log("phone works!");-----check is working*/
+      validateArea(expresions.phone, e.target, "phone");
+      break;
+    case "message":
+      /*  console.log("msg works!");-----check is working*/
+      validateArea(expresions.message, e.target, "message");
+      break;
+  }
+};
+
+/*Create function that will be re-used above */
+const validateArea = (expresion, input, area) => {
+  /*area is the value for all groups:name, email, phone, msg  */
+  if (expresion.test(input.value)) {
+    /*if everything is correct, what is inside -if- will be executed*/
+    document
+      .getElementById(`group__${area}`)
+      .classList.remove("form__group-wrong");
+    document
+      .getElementById(`group__${area}`)
+      .classList.add("form__group-right");
+    document
+      .querySelector(`#group__${area} i`)
+      .classList.add(
+        "fa-circle-check"
+      ); /*add another icon where typing is correct */
+    document
+      .querySelector(`#group__${area} i`)
+      .classList.remove("fa-circle-xmark"); /*remove xmark icon */
+    document
+      .querySelector(`#group__${area} .form__input-error`)
+      .classList.remove("form__input-error-active");
+    areas[area] = true;
+  } else {
+    document
+      .getElementById(`group__${area}`)
+      .classList.add("form__group-wrong");
+    document
+      .getElementById(`group__${area}`)
+      .classList.remove("form__group-right");
+    document
+      .querySelector(`#group__${area} i`)
+      .classList.add("fa-circle-xmark"); /*add xmark icon */
+    document
+      .querySelector(`#group__${area} i`)
+      .classList.remove("fa-circle-check"); /*remove check icon */
+    document
+      .querySelector(
+        `#group__${area} .form__input-error`
+      ) /* Show message to specify user what should be written */
+      .classList.add("form__input-error-active");
+    areas[area] = false;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validateForm);
+  input.addEventListener("blur", validateForm);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  /*check all form areas are filled*/
+  const terms = document.getElementById("terms");
+  if (
+    areas.name &&
+    areas.email &&
+    areas.phone &&
+    areas.message &&
+    terms.checked
+  ) {
+    form.reset();
+
+    document
+      .getElementById("form__msg-success")
+      .classList.add("form__msg-success-active");
+    setTimeout(() => {
+      document
+        .getElementById("form__msg-success")
+        .classList.remove("form__msg-success-active");
+    }, 5000); /*time we want this part to be executed */
+
+    /*to remove icons after submission*/
+    document.querySelectorAll(".form__group-right").forEach((icon) => {
+      icon.classList.remove("form__group-right");
+    });
+  } else {
+    document
+      .getElementById("form__msg-Displayed")
+      .classList.add("form__msg-Displayed-active");
+  }
+});
