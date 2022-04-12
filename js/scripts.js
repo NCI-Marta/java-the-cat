78999a6c8cdd3da7d86ed6faa9a138ac4b30e78d
marta <martampm.94@gmail.com>
2022-04-12 20:48:58 +0100

    responsive adjustments + adding footer

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,26 @@ $(".testimonials").slick({
   autoplaySpeed: 10000,
 });
 
+//Get the button:
+mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 jQuery(document).ready(function ($) {
   $(".fa-bars").click(function () {
     $("nav").addClass("show");
@@ -38,7 +58,8 @@ for (var i = 0; i < textOverImages.length; i++) {
 function stopPropagation(event) {
   event.stopPropagation();
 }
-//------>BUTTOM DONATE
+
+//------>BUTTON DONATE
 function donate() {
   let name = prompt("What is your name?");
   let phone = prompt("What is your phone number?");
